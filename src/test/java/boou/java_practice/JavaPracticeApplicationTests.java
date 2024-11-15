d940bf597098dd74b1d1faf1e63812a04a9d7ada
TJOEUNIS <ansdlsgur0123@gmail.com>
2024-11-15 18:22:27 +0900

    MyTest

---
+++ src/test/java/boou/java_practice/JavaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package boou.java_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
