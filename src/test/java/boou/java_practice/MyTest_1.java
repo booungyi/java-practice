a59734a81287273668a5b82820e857c39b025468
in mon <ansldsgur0123@gmail.com>
2024-11-18 18:24:13 +0900

    MyTest

--- src/test/java/boou/java_practice/MyTest_1.java
+++ src/test/java/boou/java_practice/MyTest_1.java
@@ -1,30 +1,22 @@
 package boou.java_practice;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.nio.Buffer;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Scanner;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class MyTest_1 { //클래스 안에 있는 함수 (매서드)
-/*
-https://programmers.co.kr/account/sign_in?referer=https%3A%2F%2Fschool.
-programmers.co.kr%2Flearn%2Fcourses%2F30%2Flessons%2F120805%3Flanguage%3Djava
-깨보기
 
-월요일에 지금까지 배웠던거 중에 설명할꺼 준비해오깅ㅇ
- */
     @Test
     void maxTest() {
-        assertThat(Math.max(10, 7)).isEqualTo(10);
-        assertThat(squar(5)).isEqualTo(25); //정수의 제곱
-        assertThat(getAverage(100, 100, 70)).isEqualTo(90);
+        //두 수의 합
         assertThat(add(3, 4)).isEqualTo(7);
         assertThat(add(10, -5)).isEqualTo(5);
+        //두 수중 더 큰수
+        assertThat(Math.max(10, 7)).isEqualTo(10);
+        //두 수를 곱함
+        assertThat(square(4)).isEqualTo(16); //정수의 제곱
+        //세 수의 평균
+        assertThat(getAverage(100, 100, 70)).isEqualTo(90);
         //생일구하기
         assertThat(calculateBirthYear(-4)).isEqualTo(2028);
         //isPositive is true?
@@ -70,33 +62,47 @@ programmers.co.kr%2Flearn%2Fcourses%2F30%2Flessons%2F120805%3Flanguage%3Djava
         //스트링 리스트에 저장되어잇는 스트링 개수
     }
 
-    // 함수기능: 정수 두개를 전달 받아 그합을 반환한다.
-    int squar(int n) {
-        double sum = Math.pow(n, 2);
-        return (int) sum;
-
-    }
+    /*
+    https://programmers.co.kr/account/sign_in?referer=https%3A%2F%2Fschool.
+    programmers.co.kr%2Flearn%2Fcourses%2F30%2Flessons%2F120805%3Flanguage%3Djava
+    깨보기
+    월요일에 지금까지 배웠던거 중에 설명할꺼 준비해오깅ㅇ
+     */
 
     //함수기능: 정수 두 개를 전달받아 그합을 반환한다.
     int add(int a, int b) {
         return a + b;
     }
 
-    //함수 기능: 정수 세 개를 전달받아 그 편균을 반환한다.
+    // 함수기능: 정수 하나를 전달받아 그정수를 제곱한 값을 반환한다.
+    int square(int n) {
+        return (int) Math.pow(n, 2);
+    }
+
+    //함수 기능: 정수 세 개를 전달받아 그 평균 을 반환한다.
     double getAverage(double a, double b, double c) {
         return ((a + b + c) / 3);
     }
 
     //함수 기능: 나이를 전달받아 출생 연도를 구해 반환한다.
     int calculateBirthYear(int n) {
-        return 2024 - n;
+        int ThisYear = LocalDate.now().getYear();
+        return ThisYear - n;
     }
 
     @Test
-     void 연나이테스트() {
-        assertThat(calculateBirthYear(1)).isEqualTo(LocalDate.now().getYear());
-        assertThat(calculateBirthYear(2)).isEqualTo(2023);
-        assertThat(calculateBirthYear(20)).isEqualTo(2005);
+    void typeTest() {
+        int n = 100;
+        String s = "Hello";
+        System.out.println(s.length());
+    }
+
+    @Test
+    void 연나이테스트() {
+        assertThat(calculateBirthYear(0)).isEqualTo(LocalDate.now().getYear());
+        assertThat(calculateBirthYear(2)).isEqualTo(2022);
+        assertThat(calculateBirthYear(20)).isEqualTo(2004);
+        System.out.println(calculateBirthYear(222));
     }
 
     //함수 기능: 정수 하나를 전달받아 0보다 큰지 아닌지 판단 결과를 반환한다.
@@ -124,16 +130,15 @@ programmers.co.kr%2Flearn%2Fcourses%2F30%2Flessons%2F120805%3Flanguage%3Djava
 
     //함수 기능: 세 정수를 전달받아 그중 가장 큰 수를 반환
     int getBiggest(int a, int b, int c) {
-        int sum = Math.max(a, b);
-        if (sum < c) {
-            sum = c;
+        if (Math.max(a, b) < c) {
+            return c;
         }
-        return sum;
+        return (Math.max(a, b));
     }
 
     //함수 기능 : 두 정수를 전달받아 그 중 더 작은 수를 반환
-    int min (int a, int b) {
-        int sum = Math.min(a,b);
+    int min(int a, int b) {
+        int sum = Math.min(a, b);
         return sum;
     }
 
@@ -144,20 +149,23 @@ programmers.co.kr%2Flearn%2Fcourses%2F30%2Flessons%2F120805%3Flanguage%3Djava
         }
         return n;
     }
+
     //함수 기능: 문자열 하나를 전달받아 빈 문자열인지 판단
     boolean isEmpty(String n1) {
-        if (n1 != "") {
-            return false;
+        if (n1.isEmpty()) {
+            return true;
         }
-        return true;
-        }
+        return false;
+    }
+
     //함수 스트링 2개를 이어붙이기
     String concat(String s1, String s2) {
-        return s1 + s2;
+        return s1.concat(s2);
     }
+
     //함수기능: 주민등록번호 1 이나 3 으로 시작하면 true 아니면 false
     boolean isMale(String a1) {
-        if (a1.startsWith("1")||(a1.startsWith("3"))) {
+        if (a1.startsWith("1") || (a1.startsWith("3"))) {
             return true;
         } else {
             return false;
@@ -167,16 +175,32 @@ programmers.co.kr%2Flearn%2Fcourses%2F30%2Flessons%2F120805%3Flanguage%3Djava
     //함수 기능: 스트링 리스트에 저장되어 잇는 스트링 개수
     int getLength(String a1, String a2, String a3) {
         int sum = 0;
-        if (a1 =="a") {
-            sum +=1;
-        }
-        if (a2 == "b") {
+        if (a1.equals("a")) {
             sum += 1;
         }
-        if (a3 == "c") {
+        if (a2.equals("b")) {
+            sum += 1;
+        }
+        if (a3.equals("c")) {
             sum += 1;
         }
         return sum;
+    }
+
+    @Test
+    void Testempty() {
+        String s = "123a";
+        assertThat(s.isEmpty()).isFalse(); //false (비어있는지 판별)
+        assertThat(s.equals("123a")).isTrue();//true (문자열이 같은지)
+        assertThat(s.charAt(3)).isEqualTo('a');// a  (charAt(안에 들어간 숫자번째 문자열을 문자 하나(char)로 변환) 123"a")
+        assertThat(s.toLowerCase()).isEqualTo("123a");// 222 (소문자로 변환)
+        assertThat(s.toUpperCase()).isEqualTo("123A");//  222(대문자로 변환)
+        assertThat(s.contains("22")).isFalse();// false (같은 문자열이 들어가있는지)
+        assertThat(s.indexOf(2,3)).isEqualTo(-1);// -1 <== ((찾는 문자열이 몇번째에 있는지),(시작할위치)) 없으면 -1 반환
+        assertThat(s.startsWith("1")).isTrue();//  true  (시작하는 문자열이 같은지)
+        assertThat(s.endsWith("a")).isTrue();//  true    (끝나는 문자열이 같은지)
+        assertThat(s.repeat(2)).isEqualTo("123a123a"); //22222222222222 (문자열 반복횟수)
+        assertThat(s.concat("24241")).isEqualTo("123a24241");// 123a24241 ( 문자열 끝에 추가할 문자열 )
     }
 }
 
